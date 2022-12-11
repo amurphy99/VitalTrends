62ca26ab966dd1506b7b797990eac24534803bcf
amurphy99 <amurphy62299@gmail.com>
2022-12-11 12:07:05 -0600

    Initial Commit

---
+++ murand0/murand0App.swift
@@ -0,0 +1,20 @@
+//
+//  murand0App.swift
+//  murand0
+//
+//  Created by Andrew Murphy on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct murand0App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
