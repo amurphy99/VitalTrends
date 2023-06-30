352364e3474392b808e9c189558cda68b8b29ab6
amurphy99 <amurphy62299@gmail.com>
2023-06-30 14:46:52 -0500

    Update murand0App.swift

--- murand0/murand0App.swift
+++ murand0/murand0App.swift
@@ -13,7 +13,7 @@ struct murand0App: App {
 
     var body: some Scene {
         WindowGroup {
-            UserEventsView()
+            MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
 
         }
